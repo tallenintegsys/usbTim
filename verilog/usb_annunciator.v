8fd5645102e5e8c648421b56681f51159ddd1568
Tim Allen <tim.allen@base2engineering.com>
2023-05-01 14:10:21 -0400

    show some data at the bottom

--- verilog/usb_annunciator.v
+++ verilog/usb_annunciator.v
@@ -23,15 +23,17 @@ module usb_annunciator (
 	input	din_v);
 
 // status
-reg	[9:0] ptr = 0;
+reg	[9:0] outptr = 0;
 reg 	[7:0] status [0:1023]; // DP16KD
 reg	inhibit = 0;
+reg	[9:0] inptr = 10'd348;
 
 always @(posedge clk48) begin
 	if (rst) begin
 		inhibit <= 0;
-		ptr <= 0;
-		dout_v <= 1;
+		outptr <= 0;
+		dout_v <= 1'd1;
+		inptr <= 10'd348;
 	end else begin
 		if (inhibit) begin
 			if (!inc) begin
@@ -39,40 +41,49 @@ always @(posedge clk48) begin
 			end
 			dout_v <= 0;
 		end else if (inc) begin
-			if (tx_en && ptr == 10'd22)
+			if (tx_en && outptr == 10'd22)
 				dout <= "1";
-			else if (tx_j && ptr == 10'd54)
+			else if (tx_j && outptr == 10'd54)
 				dout <= "1";
-			else if (tx_se0 && ptr == 10'd86)
+			else if (tx_se0 && outptr == 10'd86)
 				dout <= "1";
-			else if (usb_rst && ptr == 10'd118)
+			else if (usb_rst && outptr == 10'd118)
 				dout <= "1";
-			else if (transaction_active && ptr == 10'd150)
+			else if (transaction_active && outptr == 10'd150)
 				dout <= "1";
-			else if (ptr == 10'd199) begin
+			else if (outptr == 10'd199) begin
 				if (endpoint < 4'ha)
 					dout <= endpoint + "0";
 				else
 					dout <= endpoint + "(";
 			end
-			else if (direction_in && ptr == 10'd214)
+			else if (direction_in && outptr == 10'd214)
 				dout <= "1";
-			else if (setup && ptr == 10'd246)
+			else if (setup && outptr == 10'd246)
 				dout <= "1";
-			else if (data_strobe && ptr == 10'd278)
+			else if (data_strobe && outptr == 10'd278)
 				dout <= "1";
-			else if (success && ptr == 10'd310)
+			else if (success && outptr == 10'd310)
 				dout <= "1";
 			else
-				dout <= status[ptr];
+				dout <= status[outptr];
 
-			if (dout == "\014") begin
-				ptr <= 10'd4; // do it again w/o the erase screen
+			if (outptr == 10'd400) begin
+				outptr <= 10'd4; // do it again w/o the erase screen
 			end else begin
-				ptr <= ptr + 10'd1;
+				outptr <= outptr + 10'd1;
 				dout_v <= 1'b1;
 				inhibit <= 1'b1;
 			end
+		end
+
+
+		if (din_v) begin
+			if (inptr > 10'd500)
+				inptr <= 10'd348;
+		end else begin
+			inptr <= inptr + 10'd1;
+			status[inptr] <= din;
 		end
 	end //reset
 end // always
